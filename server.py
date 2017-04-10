09e9def69ab8199eae655f36bee5df34af5abc62
Renderhaf <dvir.akerman@gmail.com>
2017-04-09 22:40:25 -0700

    5000

--- server.py
+++ server.py
@@ -17,3 +17,7 @@ def index():
 @app.route("/Agar", methods=['GET', 'POST'])
 def index():
     return "Agar.html"
+
+if __name__ == "__main__":
+    port=os.environ.get('PORT') or 5000
+    app.run(host='0.0.0.0', port=int(port))
