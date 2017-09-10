d390376bb4380e788af958f978c94829ab7975e6
Renderhaf <dvir.akerman@gmail.com>
2017-09-10 00:30:33 -0700

    added Graph

--- server.py
+++ server.py
@@ -147,6 +147,10 @@ def WN():
 def GA():
     return(render_template("GoogleAds.html"))
 
+@app.route("/Graph/")
+def Graph():
+    return(render_template("Graph.html"))
+
 if __name__ == "__main__":
     port=os.environ.get('PORT') or 5000
     app.run(host='0.0.0.0', port=int(port))
