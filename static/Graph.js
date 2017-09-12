fd208fe8c5a9ffada4c59907c173ce26716ad2dd
Renderhaf <dvir.akerman@gmail.com>
2017-09-11 23:20:02 -0700

    Graph 2.0 - Added multipul Graphes support

--- static/Graph.js
+++ static/Graph.js
@@ -1,8 +1,8 @@
-var eq = document.getElementById("Eq")
+var eqs = [document.getElementById("Eq")]
 var errortext = document.getElementById("error")
 var c = document.getElementById("myCan");
 var ctx = c.getContext("2d");
-
+var cleareqs = 0
 
   var CenterLines = function(){
     ctx.fillStyle = "Gray"
@@ -20,40 +20,68 @@ var ctx = c.getContext("2d");
     ctx.fillStyle = "White"
     ctx.fillRect(0,0,c.width,c.height);
   }
-  var Main = function(){
 
+  var Add = function(){
+    var list = document.getElementById("EqList")
+    var newli = document.createElement("li")
+    var neweq = document.createElement("input")
+    neweq.setAttribute("id", "eq" + (eqs.length + 1).toString())
+
+    var newco = document.createElement("input")
+    newco.setAttribute("id", "co" + (eqs.length).toString())
+    newco.setAttribute("type", "color")
+
+    newli.appendChild(neweq);
+    newli.appendChild(newco);
+    newli.appendChild(document.createElement("br"))
+
+    list.appendChild(newli)
+
+    eqs.push(document.getElementById("eq"+(eqs.length + 1).toString()))
+  }
+
+  var Main = function(){
+    cleareqs = 0
     clearctx()
 
-    try {
-      if (eq.value != ""){
+    // try {
+        for (h=0;h<eqs.length;h++){
+          if (eqs[h].value != ""){
 
-        ctx.beginPath();
-        ctx.moveTo(map(-c.width / 2, -c.width / 2, c.width / 2,0, c.width), c.height / 2 - math.eval(eq.value,{"X" : -c.width / 20, "x" :-c.width / 20}));
+            ctx.beginPath();
+            ctx.moveTo(map(-c.width / 2, -c.width / 2, c.width / 2,0, c.width), c.height / 2 - math.eval(eqs[h].value,{"X" : -c.width / 20, "x" :-c.width / 20}));
 
-        for (i = -c.width / 2 + 1; i < c.width / 2 ; i++){
+            for (i = -c.width / 2 + 1; i < c.width / 2 ; i++){
 
-            var x = i / 10
-            var val = math.eval(eq.value,{"X" : x, "x" : x})
+                var x = i / 10
+                var val = math.eval(eqs[h].value,{"X" : x, "x" : x})
 
-            ctx.strokeStyle = document.getElementById("colorpicker").value
-            ctx.lineWidth = 3
-            ctx.lineTo(map(i, -c.width / 2, c.width / 2,0, c.width), c.height / 2 - val);
+                ctx.strokeStyle = document.getElementById("co" + h).value
+                ctx.lineWidth = 3
+                ctx.lineTo(map(i, -c.width / 2, c.width / 2,0, c.width), c.height / 2 - val);
+            }
+
+            ctx.stroke()
+
+          } else {
+
+              cleareqs ++
+
+          }
+
+
         }
 
-        ctx.stroke()
+        errortext.innerHTML = " "
 
-      } else {
-
+        if (cleareqs == eqs.length){
           clearctx()
-
-      }
-
-      errortext.innerHTML = " "
-
-    }
-    catch(err) {
-      errortext.innerHTML = err.toString()
-    }
+        }
+    // }
+    //
+    // catch(err) {
+    //   errortext.innerHTML = err.toString()
+    // }
     CenterLines()
   }
   var time_ = setInterval(Main,100)
