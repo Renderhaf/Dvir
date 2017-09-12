9294a6c04b8293dcbd28c1309b35a791909c88ef
Renderhaf <dvir.akerman@gmail.com>
2017-09-12 10:54:44 -0700

    added Docs

--- server.py
+++ server.py
@@ -151,6 +151,10 @@ def GA():
 def Graph():
     return(render_template("Graph.html"))
 
+@app.route("/Docs/")
+def Docs():
+    return(render_template("Documents.html"))
+
 if __name__ == "__main__":
     port=os.environ.get('PORT') or 5000
     app.run(host='0.0.0.0', port=int(port))
---
+++ static/Docs.js
@@ -0,0 +1,36 @@
+var text = document.getElementById("text")
+var fileInput = document.getElementById('fileInput');
+
+var Save = function(){
+  var blob = new Blob([text.value], {type: "text/plain;charset=utf-8"});
+
+  var name = prompt("File Name:")
+  if (name == "") {
+    name = "NewFile.txt"
+  } else {
+    if (name.search(".") == -1){
+      name = name + ".txt"
+    } else {
+      name = name
+    }
+  }
+    saveAs(blob, name);
+}
+
+var New = function(){
+  text.value = ""
+}
+
+fileInput.addEventListener('change', function(e) {
+    var file = fileInput.files[0];
+    var textType = /text.*/;
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+          text.value = reader.result;
+    }
+
+    reader.readAsText(file);
+
+  });
