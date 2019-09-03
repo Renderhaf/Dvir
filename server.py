6651c692277c05927e79c6661d1b2f630b5cfed7
Renderhaf <dvir.akerman@gmail.com>
2019-09-03 22:51:07 +0300

    Added Admin Login and msg reset

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-import os
-from flask import Flask, render_template
-from flask import request
-from flask.json import jsonify
 import json
+import os
 import smtplib
+import time
+
 import requests
+from flask import Flask, render_template, request, flash, make_response
+from flask.json import jsonify
+import hashlib
 
 app = Flask(__name__)
 
@@ -15,27 +17,32 @@ changes = []
 moves = {}
 messages = {}
 
-def Send(To,Content):
+
+def Send(To, Content):
     print("Sending...")
-    mail = smtplib.SMTP("smtp.gmail.com",587)
+    mail = smtplib.SMTP("smtp.gmail.com", 587)
     mail.ehlo()
     # mail.starttls()
     mail.login("smtptestspm@gmail.com", "Testone123")
-    mail.sendmail("Mail",To,Content)
+    mail.sendmail("Mail", To, Content)
     mail.close()
+
 
 @app.route("/")
 def index():
     return render_template("newindex.html")
 
+
 @app.route("/Agar/", methods=['GET', 'POST'])
 def Agar():
     if request.method == 'POST':
-            print(request.form)
-            moves[request.form["name"]] = {"x":request.form["x"],"y":request.form["y"],"health":request.form["health"],"name":request.form["name"]}
-            return jsonify(moves)
+        print(request.form)
+        moves[request.form["name"]] = {"x": request.form["x"], "y": request.form["y"],
+                                       "health": request.form["health"], "name": request.form["name"]}
+        return jsonify(moves)
     else:
         return render_template("Agar.html")
+
 
 @app.route("/Spot/", methods=['GET', 'POST'])
 def Spot():
@@ -53,54 +60,31 @@ def Spot():
         print("Returning render canavs")
         return render_template('Spot.html')
 
+
 @app.route("/GOL/")
 def GOL():
     return render_template("GOL.html")
+
 
 @app.route("/Cam/")
 def Cam():
     return(render_template("Cam.html"))
 
 
-@app.route("/MailSender/", methods=['GET', 'POST'])
-def MailSender():
-    if request.method == 'POST':
-        to = request.form["To"]
-        title = request.form["Title"]
-        print(to,title)
-        if str(to)[0] == "*":
-            to = to[1:]
-            for i in range(10):
-                Send(to,title)
-            return "Got *"
-        elif str(to)[0] == "#":
-            to = to[1:]
-            for i in range(50):
-                Send(to,title)
-            return "Got #"
-        elif str(to)[0] == "^":
-            to = to[1:]
-            for i in range(100):
-                Send(to,title)
-            return "Got ^"
-        else:
-            Send(to,title)
-        return "Got Message"
-    else:
-        return(render_template("MS.html"))
-
-
 @app.route("/PianoThing/")
 def Piano():
     return(render_template("Piano.html"))
+
 
 @app.route("/Donate/")
 def donate():
     return(render_template("donate.html"))
 
+
 @app.route("/Cap/")
 def Cap():
     return(render_template("CAP.html"))
+
 
 @app.route("/ChatRoom/", methods=['GET', 'POST'])
 def CHRM():
@@ -109,29 +93,26 @@ def CHRM():
         # print(messages)
         if request.form["Send"] == "True":
             if request.form["Group"] in messages:
-                if request.form["Text"] == "Admin : ClearMSG": #Clear Command
-                    messages = {}
-                else:
-                    messages[request.form["Group"]].append(request.form["Text"]) #Main Send
-            else: #Prevent empty chat bug
+                messages[request.form["Group"]].append(request.form["Text"])  # Main Send
+            else:  # Prevent empty chat bug
                 messages[request.form["Group"]] = []
                 messages[request.form["Group"]].append(request.form["Text"])
 
             return request.form["Text"]
 
         elif request.form["Send"] == "False":
-            if request.form["JC"] == "J": #Join Chat
+            if request.form["JC"] == "J":  # Join Chat
                 if request.form["Group"] in messages:
                     return request.form["Group"]
                 else:
                     return jsonify("-1")
-            elif request.form["JC"] == "C": #Create Chat
+            elif request.form["JC"] == "C":  # Create Chat
                 if request.form["Group"] in messages:
                     return jsonify("-1")
                 else:
                     messages[request.form["Group"]] = []
                     return request.form["Group"]
-            else: #Update messages
+            else:  # Update messages
                 if request.form["Group"] in messages:
                     return jsonify(messages[request.form["Group"]])
                 else:
@@ -139,21 +120,26 @@ def CHRM():
     else:
         return(render_template("ChatHTTP.html"))
 
+
 @app.route("/WhiteNoise/")
 def WN():
     return(render_template("WhiteNoise.html"))
+
 
 @app.route("/GA/")
 def GA():
     return(render_template("GoogleAds.html"))
 
+
 @app.route("/Graph/")
 def Graph():
     return(render_template("Graph.html"))
 
+
 @app.route("/NPS/")
 def Docs():
     return(render_template("NPS.html"))
+
 
 @app.route("/Crypto/", methods=['GET', 'POST'])
 def Crypto():
@@ -169,50 +155,93 @@ def Crypto():
     else:
         return(render_template("Crypto.html"))
 
+
 @app.route("/PicTxt/")
 def PicTxt():
     return(render_template("PictureText.html"))
+
 
 @app.route("/CryptoCount/")
 def CryptoC():
     return(render_template("CryptoCounter.html"))
 
+
 @app.route("/TABS/")
 def TABS():
     return(render_template("TabOpener.html"))
+
 
 @app.route("/Mine/")
 def Mine():
     return(render_template("MoneroMine.html"))
 
+
 @app.route("/jsSandbox/")
 def JsBox():
     return(render_template("jsSandBox.html"))
+
 
 @app.route("/PaintX")
 def PaintX():
     return(render_template("PaintX.html"))
 
+
 @app.route("/Seek")
 def Seek():
     return(render_template("Seek.html"))
+
 
 @app.route("/Pend")
 def Pend():
     return(render_template("pend.html"))
 
+
 @app.route("/PartAtt")
 def PartAtt():
     return(render_template("particals.html"))
+
 
 @app.route("/DragNDrop")
 def DragNDrop():
     return(render_template("DragNDrop.html"))
 
+
 @app.route("/PIDRect")
 def PIDRect():
     return(render_template("PIDmouseseek.html"))
 
+
+@app.route("/Admin", methods=['GET', 'POST'])
+def AdminPage():
+    if request.method == 'POST':
+        server_passhash = os.getenv(
+            'PASS_HASH') 
+        if server_passhash is None:
+            server_passhash = ""
+        if "pass" in request.form:
+            time.sleep(0.25)
+            passhash = hashlib.sha256(
+                request.form["pass"].encode()).hexdigest()
+            if server_passhash == passhash:
+                resp = make_response(render_template("adminpage.html"))
+                resp.set_cookie("secret", os.getenv(
+                    "SPECIAL_KEY"))
+                return resp
+            else:
+                return render_template("Login.html", badpass="Wrong Password!")
+    else:
+        return render_template("Login.html")
+
+@app.route("/AdminActions/<CMD>", methods=['POST'])
+def AdminCMD(CMD):
+    global messages
+    if "secret" not in request.cookies or request.cookies.get("secret") != os.getenv("SPECIAL_KEY"):
+        return jsonify("BAD COOKIE!")
+    if CMD == "RESET_MSGS":
+        messages = {}
+        return render_template("adminpage.html", msg="MSG RESET")
+
+
 if __name__ == "__main__":
-    port=os.environ.get('PORT') or 5000
+    port = os.environ.get('PORT') or 5000
     app.run(host='0.0.0.0', port=int(port))
